5339d310a4522747853cb2eb822a0b90a348c2a7
jchasse <jonathan.r.chasse@gmail.com>
2020-11-17 13:56:13 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import App from './App';
 import manageBand from './reducers/manageBand'
 
-const store = createStore(manageBand)
-
+const initialState = {bands: [{name: "Van Halen"}, {name: "ACDC"}]}
+const store = createStore(manageBand, initialState, composeWithDevTools())
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-);
+)
